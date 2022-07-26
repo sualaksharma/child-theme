b09ba77295ae6ad4d56c293cd4432202cee34d3c
sualaksharma <73638314+sualaksharma@users.noreply.github.com>
2022-07-26 15:53:54 +0530

    Add files via upload

--- functions.php
+++ functions.php
@@ -8,8 +8,10 @@ wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 
 }
 function create_posttype() {
+  
   register_post_type( 'wpll_movies',
     array(
+      
       'labels' => array(
         'name' => __( 'Movies' ),
         'singular_name' => __( 'Movie' )
@@ -25,15 +27,28 @@ function create_posttype() {
       'rewrite' => array('slug' => 'Movies'),
     )
   );
+  add_post_meta(1, 'age', 25);
 }
 add_action( 'init', 'create_posttype' );
-
+add_post_meta(38, "my_test_field", "value",true);
 add_filter('the_title', 'filter_example');
 function filter_example($title) {
 	return $title;
 }
+add_post_meta(1,'age', 25);
 
 
+function smallenvelop_widgets_init() {
+  register_sidebar( array(
+      'name' => __( 'Header Sidebar', 'smallenvelop' ),
+      'id' => 'header-sidebar',
+      'before_widget' => '<div>',
+      'after_widget' => '</div>',
+      'before_title' => '<h1>',
+      'after_title' => '</h1>',
+  ) );
+}
+add_action( 'widgets_init', 'smallenvelop_widgets_init' );
 
 
 ?>
